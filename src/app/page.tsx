2d0d2f33912658aefd9aa17b0b22d1e5c3bb5caa
sajeek0944 <sajeel6y4z@gmail.com>
2024-11-18 23:16:00 +0500

    class 10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
         <div className="sm:flex md:w-full">
 
           <div className=" w-[95%] place-items-center my-6  mx-3 bg-white hover:shadow-md hover:shadow-slate-700 py-3 rounded-md sm:hover:shadow-slate-900">
-            <Image src={"/picture/figma.jfif"} alt="" width={370} height={100}  className="w-5/6"/>
+            <Image src={"/picture/figma.webp"} alt="" width={370} height={100}  className="w-5/6"/>
             <p className="text-xl font-semibold py-3">Figma </p>
             <p className="font-semibold ">RS : 2000</p>
             <button  className="font-semibold border- border-black w-28 h-8 bg-blue-500 my-3 hover:w-32 hover:h-9"><p>BUY NOW</p></button>
           </div>
 
           <div className=" w-[95%] place-items-center my-6  mx-3 bg-white hover:shadow-md hover:shadow-slate-700 py-3 rounded-md sm:hover:shadow-slate-900">        
-            <Image src={"/picture/figma.jfif"} alt="" width={370} height={100}  className="w-5/6"/>
+            <Image src={"/picture/figma.webp"} alt="" width={370} height={100}  className="w-5/6"/>
             <p className="text-xl font-semibold py-3">Figma</p>
             <p className="font-semibold ">RS : 2000</p>
             <button  className="font-semibold border- border-black w-28 h-8 bg-blue-500 my-3 hover:w-32 hover:h-9"><p>BUY NOW</p></button>
@@ -26,7 +26,7 @@ export default function Home() {
         </div>
 
         <div className=" w-[95%] place-items-center my-6  mx-3 bg-white hover:shadow-md hover:shadow-slate-700 py-3 sm:w-[45%] rounded-md sm:hover:shadow-slate-900 md:w-[44%]">  
-          <Image src={"/picture/figma.jfif"} alt="" width={370} height={100}  className="w-5/6"/>
+          <Image src={"/picture/figma.webp"} alt="" width={370} height={100}  className="w-5/6"/>
           <p className="text-xl font-semibold py-3">Figma</p>
           <p className="font-semibold ">RS : 2000</p>
           <button  className="font-semibold border- border-black w-28 h-8 bg-blue-500 my-3 hover:w-32 hover:h-9"><p>BUY NOW</p></button>
